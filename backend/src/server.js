1480da08fd3e81ade9814fe04a8f099981a472ef
Katta Narender Naidu <narendernaidu35@gmail.com>
2025-09-05 15:08:56 +0530

    prepared for the deployment

--- backend/src/server.js
+++ backend/src/server.js
@@ -4,13 +4,23 @@ import { connectDb } from "./config/db.js";
 import dotenv from "dotenv";
 import ratelimiter from "./middleware/rateLimiter.js";
 import cors from "cors";
+import path from "path";
 dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5001
+const __dirname = path.resolve();
 app.use(express.json());
-app.use(cors({origin: "http://localhost:5173",}));
+if(process.env.NODE_ENV !== "production") {
+    app.use(cors({origin: "http://localhost:5173",}));
+}
 app.use(ratelimiter);
 app.use("/api/notes",notesRoutes);
+if(process.env.NODE_ENV === "production") {
+    app.use(express.static(path.join(__dirname,"../frontend/dist")))
+    app.get("*",(req,res)=>{
+        res.sendFile(path.join(__dirname,"../frontend","dist","index.html"))
+    })
+}
 connectDb().then(()=>{
     app.listen(PORT,()=>{
         console.log("The port is listening on port 5001");
