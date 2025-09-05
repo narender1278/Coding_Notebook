1201325c3b33178c35b15bfcc0c08c91d05c94b4
Katta Narender Naidu <narendernaidu35@gmail.com>
2025-09-05 16:37:09 +0530

    third commit

--- backend/src/server.js
+++ backend/src/server.js
@@ -21,8 +21,8 @@ if(process.env.NODE_ENV === "production") {
         res.sendFile(path.join(__dirname,"../frontend","dist","index.html"))
     })
 }
-connectDb().then(()=>{
-    app.listen(PORT,()=>{
-        console.log("The port is listening on port 5001");
-    });
+connectDb().then(() => {
+  app.listen(PORT, () => {
+    console.log(`Server is listening on port ${PORT}`);
+  });
 });
