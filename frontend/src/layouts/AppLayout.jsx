8702b9c6e88af98810c23ac232004e9ef060bab5
narender <narendernaidu.k@tnschools.gov.in>
2026-02-22 20:42:55 +0530

    changes final

--- frontend/src/layouts/AppLayout.jsx
+++ frontend/src/layouts/AppLayout.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import Navbar from "../components/Navbar";
-import Sidebar from "../components/SideBar";
+import Sidebar from "../components/Sidebar";
 
 
 const Layout = ({ children }) => {
